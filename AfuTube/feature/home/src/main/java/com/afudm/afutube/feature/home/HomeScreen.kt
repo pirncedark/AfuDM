b1421f37dd883236b8607087303c39b882f24f9e
afuuu <furkanu@gmail.com>
2026-09-22 23:57:51 +0300

    fix(android): add missing MediaInfo import in HomeScreen.kt

--- AfuTube/feature/home/src/main/java/com/afudm/afutube/feature/home/HomeScreen.kt
+++ AfuTube/feature/home/src/main/java/com/afudm/afutube/feature/home/HomeScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.afudm.afutube.core.extractor.MediaInfo
 import com.afudm.afutube.core.theme.AfuColors
 
 @OptIn(ExperimentalMaterial3Api::class)
